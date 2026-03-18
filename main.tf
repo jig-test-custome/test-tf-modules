e8a2486659b675142e4df80fefaae2808c4e25f1
K <39096245+KishtheBeast@users.noreply.github.com>
2026-03-18 18:58:58 -0400

    Initial content

---
+++ main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "6.1.1"
+
+  identifier = "demodb"
+
+  engine            = "mysql"
+  engine_version    = "8.0.32"
+  instance_class    = "db.t3.large"
+  allocated_storage = 5
+
+  db_name  = "demodb"
+  username = "user"
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  vpc_security_group_ids = ["sg-12345678"]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # DB subnet group
+  create_db_subnet_group = true
+  subnet_ids             = ["subnet-12345678", "subnet-87654321"]
+
+  # DB parameter group
+  family = "mysql8.0"
+
+  # DB option group
+  major_engine_version = "8.0"
+
+  # Database Deletion Protection
+  deletion_protection = false
+
+  parameters = [
+    {
+      name  = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name  = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
+}
